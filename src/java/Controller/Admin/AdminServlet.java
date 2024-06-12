25f221e99453d0e8103bdba7e31224e41fe19307
Hlinh_wfil <hoailinh.dienban@gmail.com>
2024-06-12 21:50:48 +0700

    Add DAO and Model

---
+++ src/java/Controller/Admin/AdminServlet.java
@@ -0,0 +1,35 @@
+package Controller.Admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class AdminServlet extends HttpServlet {
+
+    private static final String ADMIN = "view/jsp/admin/admin_home.jsp";
+    private final static String ORDER_DETAIL_PAGE = "view/jsp/admin/admin_order_detail.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AdminServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet AdminServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+
+}
